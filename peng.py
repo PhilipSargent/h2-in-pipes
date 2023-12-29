cdd61eae92e7146a25a4e06d78f55c0d8362a645
Philip Sargent <philip.sargent@gmail.com>
2023-12-29 15:23:08 +0000

    tuned 'our mix' to be a valid NTScompatible gas

--- peng.py
+++ peng.py
@@ -9,7 +9,7 @@ gas in the UK distribution grid with hydrogen.
 
 For general information, commercial natural gas typically contains 85 to 90 percent methane, with the remainder mainly nitrogen and ethane, and has a calorific value of approximately 38 megajoules (MJ) per cubic metre
 
-Wobbe Number (WN) (i) ≤51.41 MJ/m3, and  (ii)  ≥47.20 MJ/m3
+Wobbe Number (WN) (i) ≤51.41 MJ/m³, and  (ii)  ≥47.20 MJ/m³
 https://www.nationalgas.com/data-and-operations/quality
 
 UNITS: bar, K, litres
@@ -33,8 +33,9 @@ T273 = 273.15
 # for cryogenic vapour pressure. This FAILS for room temperature work, producing infinities. Use omega instead.
 # possibly re-visit using the data from Jaubert et al. on H2 + n-butane.
 
-# Hc is standard enthalpy of combustion at 298K, i.e. HHV,  in MJ/mol (negative sign not used). Data from Wikipedia
-# Wb is Wobbe index: MJ/m3
+# Hc is standard enthalpy of combustion at 298K, i.e. HHV,  in MJ/mol (negative sign not used). 
+#     Data from Wikipedia & https://www.engineeringtoolbox.com/standard-heat-of-combustion-energy-content-d_1987.html
+# Wb is Wobbe index: MJ/m³
 # RD is relative density (air is  1)
 # Vs is viscosity and temp. f measurement as tuple (microPa.s, K, exponent(pressure))
 # All viscosities from marcia l. huber and allan h. harvey,
@@ -46,18 +47,18 @@ T273 = 273.15
 gas_data = {
     'H2': {'Tc': 33.2, 'Pc': 13.0, 'omega': -0.22, 'Mw':2.015, 'Vs': (8.9,300, 0.692), 'Hc':0.286},
     'CH4': {'Tc': 190.56, 'Pc': 45.99, 'omega': 0.01142, 'Mw': 16.0428, 'Vs': (11.1,300, 1.03), 'Hc':0.891},
-    'C2H6': {'Tc': 305.32, 'Pc': 48.72, 'omega': 0.099, 'Mw': 30.07, 'Vs': (9.4,300, 0.87)}, # 
-    'C3H8': {'Tc': 369.15, 'Pc': 42.48, 'omega': 0.1521, 'Mw': 44.096, 'Vs': (8.2,300, 0.93)}, # https://www.engineeringtoolbox.com/propane-d_1423.html
+    'C2H6': {'Tc': 305.32, 'Pc': 48.72, 'omega': 0.099, 'Mw': 30.07, 'Vs': (9.4,300, 0.87), 'Hc':1.561}, # 
+    'C3H8': {'Tc': 369.15, 'Pc': 42.48, 'omega': 0.1521, 'Mw': 44.096, 'Vs': (8.2,300, 0.93), 'Hc':2.22}, # https://www.engineeringtoolbox.com/propane-d_1423.html
     'nC4': {'Tc': 425, 'Pc': 38,  'omega': 0.20081, 'Mw': 58.1222, 'Vs': (7.5,300, 0.950), 'Hc':2.8781}, # omega http://www.coolprop.org/fluid_properties/fluids/n-Butane.html https://www.engineeringtoolbox.com/butane-d_1415.html 
     'iC4': {'Tc': 407.7, 'Pc': 36.5, 'omega': 0.1835318, 'Mw': 58.1222, 'Vs': (7.5,300, 0.942), 'Hc':2.86959}, # omega  http://www.coolprop.org/fluid_properties/fluids/IsoButane.html https://webbook.nist.gov/cgi/cbook.cgi?ID=C75285&Mask=1F https://webbook.nist.gov/cgi/cbook.cgi?Name=butane&Units=SI Viscocity assumed same as nC4
-    'nC5': {'Tc': 469.8, 'Pc': 33.6, 'omega': 0.251032, 'Mw': 72.1488, 'Vs': (6.7,300, 1.0)}, # omega http://www.coolprop.org/fluid_properties/fluids/n-Pentane.html     
-    'iC5': {'Tc': 461.0, 'Pc': 33.8, 'omega': 0.2274, 'Mw': 72.1488, 'Vs': (6.7,300, 0.94)}, # omega http://www.coolprop.org/fluid_properties/fluids/Isopentane.html  Viscocity assumed same as nC5 
+    'nC5': {'Tc': 469.8, 'Pc': 33.6, 'omega': 0.251032, 'Mw': 72.1488, 'Vs': (6.7,300, 1.0), 'Hc':3.509}, # omega http://www.coolprop.org/fluid_properties/fluids/n-Pentane.html     
+    'iC5': {'Tc': 461.0, 'Pc': 33.8, 'omega': 0.2274, 'Mw': 72.1488, 'Vs': (6.7,300, 0.94), 'Hc':3.509}, # omega http://www.coolprop.org/fluid_properties/fluids/Isopentane.html  Viscocity assumed same as nC5 
     
-    'neoC5': {'Tc': 433.8, 'Pc': 31.963, 'omega': 0.1961, 'Mw': 72.1488, 'Vs': (6.9326,300, 0.937)},
+    'neoC5': {'Tc': 433.8, 'Pc': 31.963, 'omega': 0.1961, 'Mw': 72.1488, 'Vs': (6.9326,300, 0.937), 'Hc':3.509},
     # https://webbook.nist.gov/cgi/cbook.cgi?ID=C463821&Units=SI&Mask=4#Thermo-Phase
     # omega from http://www.coolprop.org/fluid_properties/fluids/Neopentane.html
     
-    'C6':  {'Tc': 507.6, 'Pc': 30.2, 'omega': 0.1521, 'Mw': 86.1754, 'Vs': (8.6,400, 1.03)}, # omega is 0.2797 isohexane    
+    'C6':  {'Tc': 507.6, 'Pc': 30.2, 'omega': 0.1521, 'Mw': 86.1754, 'Vs': (8.6,400, 1.03), 'Hc':4.163}, # omega is 0.2797 isohexane    
     'CO2': {'Tc': 304.2, 'Pc': 73.8, 'omega': 0.228, 'Mw': 44.01, 'Vs': (15.0,300, 0.872), 'Hc':0}, # https://en.wikipedia.org/wiki/Acentric_factor
     'H2O': {'Tc': 647.1, 'Pc': 220.6, 'omega': 0.344292, "Mw": 18.015, 'Vs': (9.8,300, 1.081), 'Hc':0}, # https://link.springer.com/article/10.1007/s10765-020-02643-6/tables/1
     'N2': {'Tc': 126.21, 'Pc': 33.958, 'omega': 0.0372, 'Mw':28.013, 'Vs': (17.9,300, 0.658), 'Hc':0}, #  omega http://www.coolprop.org/fluid_properties/fluids/Nitrogen.html
@@ -73,9 +74,9 @@ gas_data = {
 gas_mixtures = {
     'GG': {'CH4': 0.813, 'C2H6': 0.0285, 'C3H8': 0.0037, 'nC4': 0.0014, 'nC5': 0.0004, 'C6': 0.0006, 'CO2': 0.0089, 'N2': 0.1435, 'O2': 0}, # Groeningen gas https://en.wikipedia.org/wiki/Groningen_gas_field
     
-    #'Wobbe mix': {'CH4': 0.9,  'C3H8': 0.04,  'N2': 0.06}, # wobbe central, not a real natural gas  https://www.gasgovernance.co.uk/sites/default/files/ggf/Impact%20of%20Natural%20Gas%20Composition%20-%20Paper_0.pdf
+    'Our mix': {'CH4': 0.92,  'C3H8': 0.04, 'CO2': 0.04 }, # wobbe central, not a real natural gas  https://www.gasgovernance.co.uk/sites/default/files/ggf/Impact%20of%20Natural%20Gas%20Composition%20-%20Paper_0.pdf
     
-    #'mix6': {'CH4': 0.8, 'C2H6': 0.05, 'C3H8': 0.03, 'CO2': 0.02, 'N2': 0.10}, # ==mix6 from      https://backend.orbit.dtu.dk/ws/files/131796794/FPE_D_16_00902R1.pdf - no, somewhere else..
+    'mix6': {'CH4': 0.8, 'C2H6': 0.05, 'C3H8': 0.03, 'CO2': 0.02, 'N2': 0.10}, # ==mix6 from      https://backend.orbit.dtu.dk/ws/files/131796794/FPE_D_16_00902R1.pdf - no, somewhere else..
 
     'NTS79': {'CH4': 0.9363, 'C2H6': 0.0325, 'C3H8': 0.0069, 'nC4': 0.0027, 'CO2': 0.0013, 'N2': 0.0178, 'He': 0.0005, 'nC5': 0.002}, # https://en.wikipedia.org/wiki/National_Transmission_System
     # This NTS composition from Wikipedia actually comes from 1979 !  Cassidy, Richard (1979). Gas: Natural Energy. London: Frederick Muller Limited. p. 14.
@@ -93,7 +94,7 @@ gas_mixtures = {
     # But ALSO adding 2.5% moisture to the air and normalising
 }
 
-"""reduce the lower limit for Wobbe Index from 47.2 MJ/m3  to 46.50 MJ/m3 was approved by HSE. 
+"""reduce the lower limit for Wobbe Index from 47.2 MJ/m³  to 46.50 MJ/m³ was approved by HSE. 
 This shall enter into force from 6 April 2025
 "Gas Ten Year Statement December 2023"
 https://www.nationalgas.com/document/144896/download
@@ -113,7 +114,7 @@ for g in ng:
 gas_mixtures['NatGas+20%H2'] = fifth
 
 
-print(f"NatGas gas composition")
+print(f"NatGas gas composition: Duchowny22, doi:10.1016/j.egyr.2022.02.289")
 nts = gas_mixtures["NatGas"]
 for f in nts:
     print(f"{f:5} {nts[f]*100:7.4f} %")
@@ -547,25 +548,32 @@ def get_density(mix, p, T):
 
 def get_Hc(g):
     """If the data is there, return the standard heat of combustion, 
-    but in MJ/m3 not MJ/mol"""
+    but in MJ/m³ not MJ/mol"""
     if g in gas_mixture_properties and 'Hc' in gas_mixture_properties[g]:
         hc = gas_mixture_properties[g]['Hc']
     elif g in gas_data and 'Hc' in gas_data[g]:
         hc = gas_data[g]['Hc']
     else:
-        hc = None
-    # hc is in MJ/mol, so we need to divide by the molar volume at STP (0 degrees C, 1 atm) in m3
+        hc = 0
+        composition = gas_mixtures[g]
+        for pure_gas, x in composition.items():
+            # Linear mixing rule for volume factor
+            hc += x * gas_data[pure_gas]['Hc']
+    # hc is in MJ/mol, so we need to divide by the molar volume at STP (0 degrees C, 1 atm) in m³
     Mw = do_mm_rules(g)/1000 # Mw now in kg/mol not g/mol
-    ϱ_0 = get_density(g, 1.01325, 273.15) # in kg/m3
-    molar_volume = ϱ_0 / Mw # in m3/mol
+    ϱ_0 = get_density(g, 1.01325, 273.15) # in kg/m³
+    molar_volume = Mw / ϱ_0  # in m³/mol
     
     if hc:
-        return hc/molar_volume
-    return None
+        return molar_volume, hc/molar_volume, hc
+    else:
+        return molar_volume, None, None
     
 def print_wobbe(g, p, T):
-    """HHV and Wobbe much be in MJ/m3, but at zero C and 1 atm, not p and T as given
+    """HHV and Wobbe much be in MJ/m³, but at zero C and 1 atm, not p and T as given
+    UK NTS WObbe limits from     https://www.nationalgas.com/data-and-operations/quality
     """
+    best = (47.20 + 51.41) / 2
     ϱ = get_density(g, p, T)
     
     # Wobbe is at STP
@@ -574,15 +582,30 @@ def print_wobbe(g, p, T):
  
     wobbe_factor_ϱ = 1/np.sqrt(ϱ_0/ϱ_air)
     
-    hc = get_Hc(g)
+    mv, hcmv, hc = get_Hc(g)
+    
+    # yeah, yeah: 'f' strings are great
     if hc:
-        w = wobbe_factor_ϱ * hc
-        w = f"{w:.5f}"
+        w = wobbe_factor_ϱ * hcmv
+        niceness = 100*w/best - 100
+        flag = f"{'nice':^8} {niceness:+.1f} %"
+        if w < 47.20:
+            flag = "TOO LOW"
+        if w  > 51.41:
+            flag = "TOO HIGH"
+
+        w = f"{w:>.5f}"
+        hc = f"{hc:^11.3f}"
+        hcmv = f"{hcmv:^11.4f}"
     else:
-        w = " - "      
-        
+        w = f"{'-':^10}"
+        hc = f"{'-':^11}"
+        hcmv = f"{'-':^11}"
+        flag = f"{'-':^8}"
+   
+    
     mm = do_mm_rules(g) # mean molar mass
-    print(f"{g:15} {mm:6.3f} {ϱ:.5f}   {wobbe_factor_ϱ:.5f}   {w}")
+    print(f"{g:15} {mm:6.3f}  {ϱ:.5f}   {hc} {mv:.7f} {hcmv}{wobbe_factor_ϱ:11.5f}   {w} {flag}")
     
 # ---------- ----------main program starts here---------- ------------- #
 
@@ -621,14 +644,15 @@ pressure =  Atm + dp/1000 # 1atm + 47.5 mbar, halfway between 20 mbar and 75 mba
 T = 273.15 + tp
 # Print the densities at 15 C  - - - - - - - - - - -
 
-print(f"Mw and density of gases (kg/m³)at T={tp:.1f}°C and P={dp:.1f} mbar above 1 atm, i.e. P={pressure:.5f} bar")
-print(f"{'gas':15} {'Mw':6} {'ϱ':5}   {'W_factor_ϱ':5}   Wobbe No. ")
+print(f"\nDensity of gas at (kg/m³)at T={tp:.1f}°C and P={dp:.1f} mbar above 1 atm, i.e. P={pressure:.5f} bar")
+print(f"Hc etc. all at 0°C and 1 atm = {Atm} bar. Wobbe limit is  47.20 to 51.41")
+print(f"W_factor_ϱ =  1/(sqrt(ϱ/ϱ(air))) ")
+print(f"{'gas':13}{'Mw(g/mol)':6}  {'ϱ(kg/m³)':5} {'Hc(MJ/mol)':11} {'MV₀(m³/mol)':11} {'Hc(MJ/m³)':11}{'W_factor_ϱ':11} Wobbe(MJ/m³) ")
 for g in gas_mixtures:
     print_wobbe(g, pressure, T)
 for g in ["H2", "CH4"]:
     print_wobbe(g, pressure, T)
 
-print(f"3rd column is W_factor =  1/(sqrt(ϱ/ϱ(air))) at 0°C and 1 atm ({Atm} bar)")
 
 # Plot the compressibility  - - - - - - - - - - -
 
@@ -780,7 +804,7 @@ for g in ["H2", "CH4", "N2", "O2"]:
 
 plt.title(f'Density / Dynamic Viscosity vs Temperature at {pressure} bar')
 plt.xlabel('Temperature (°C)')
-plt.ylabel('Density/ Dynamic Viscosity (kg/m3)/(μPa.s) ')
+plt.ylabel('Density/ Dynamic Viscosity (kg/m³)/(μPa.s) ')
 plt.legend()
 plt.grid(True)
 
